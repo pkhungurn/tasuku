47037c6ea0a5884a2c09c6b6da7a18078a64929f
dragonmeteor <dragonmeteor@gmail.com>
2023-01-01 04:10:28 +0900

    examples for python

---
+++ python/src/example/run.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import logging
+
+sys.path.append(os.path.join(os.getcwd(), "src"))
+
+from pytasuku import *
+from example import tasks
+
+
+def replace_sep_with_slash(path):
+    comps = path.split(os.path.sep)
+    return "/".join(comps)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Usage: python src/example/run.py <task-name-1> <task-name-2> ...")
+        sys.exit(0)
+
+    logging.basicConfig(level=logging.INFO)
+    workspace = Workspace()
+    tasks.define_tasks(workspace)
+
+    workspace.start_session()
+    for arg in sys.argv[1:]:
+        arg = replace_sep_with_slash(arg)
+        workspace.run(arg)
+    workspace.end_session()
